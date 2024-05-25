c5d7eca337c76cd20489914576fc58f9078ae3a0
Alfayed Baksh <baksha97@gmail.com>
2024-05-24 21:26:25 -0400

    update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 # MacOS System
 # Remove Message of the day prompt
-touch $HOME/.hushlogin
 # Show hidden files in finder
 defaults write com.apple.finder AppleShowAllFiles YES
 
@@ -10,20 +9,48 @@ defaults write com.apple.finder AppleShowAllFiles YES
 
 # Install all homebrew packages
 # TODO: use a brewfile https://github.com/ahmedelgabri/dotfiles/blob/master/homebrew/Brewfile
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    brew install "$line"
-done < "./brew.txt"
+# while IFS='' read -r line || [[ -n "$line" ]]; do
+#     brew install "$line"
+# done < "./brew.txt"
 
-# SSH config
-stow ssh -t $HOME/
+# Install Oh My Zsh if not installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
-stow bash -t $HOME
+# Install Powerlevel10k theme if not installed
+if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH/custom/themes/powerlevel10k
+fi
+
+# Install Zsh plugins if not installed
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+fi
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+fi
+
+# Install SDKMAN! if not installed
+if [ ! -d "$HOME/.sdkman" ]; then
+  curl -s "https://get.sdkman.io" | bash
+  source "$HOME/.sdkman/bin/sdkman-init.sh"
+fi
+
+
 rm $HOME/.zshrc
-stow zsh -t $HOME
-
-stow vscode -t $HOME
+stow zsh -t $HOME --adopt
+stow powerlevel10k -t $HOME --adopt
+stow vscode -t $HOME --adopt
 ln -s "$HOME/dotfiles/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
 
+
 # git
-stow git -t $HOME/
+stow git -t $HOME/ --adopt
 git config --global core.excludesfile $HOME/.gitignore
+
+# Source .zshrc
+source ~/.zshrc
+
+echo "Dotfiles installation complete!"

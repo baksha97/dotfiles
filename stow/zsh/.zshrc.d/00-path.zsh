c8e7101a5020dd69995f6c3ba3f31604989f1609
Alfayed Baksh <baksha97@gmail.com>
2026-03-09 12:04:00 -0400

    feat: update PNPM_HOME path to use HOME variable for better portability

--- stow/zsh/.zshrc.d/00-path.zsh
+++ stow/zsh/.zshrc.d/00-path.zsh
@@ -1,7 +1,7 @@
 [[ -d "$HOME/.local/bin" ]] && export PATH="$HOME/.local/bin:$PATH"
 [[ -d "/opt/homebrew/anaconda3/bin" ]] && export PATH="/opt/homebrew/anaconda3/bin:$PATH"
 
-export PNPM_HOME="/Users/travis/Library/pnpm"
+export PNPM_HOME="$HOME/Library/pnpm"
 [[ -d "$PNPM_HOME" ]] && export PATH="$PNPM_HOME:$PATH"
 
 

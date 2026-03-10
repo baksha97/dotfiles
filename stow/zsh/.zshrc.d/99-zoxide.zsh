1d929cd5016b100c39336a4fe48bba8ef3b3a811
Alfayed Baksh <baksha97@gmail.com>
2026-03-10 14:03:31 -0400

    WSL2 Support (#21)
    * disable z for wsl
    
    * disable z for wsl
    
    * disable z for wsl only

--- stow/zsh/.zshrc.d/99-zoxide.zsh
+++ stow/zsh/.zshrc.d/99-zoxide.zsh
@@ -1,4 +1,7 @@
 # zoxide — must run after compinit
+# Only init zoxide if NOT on WSL
 if command -v zoxide &>/dev/null; then
-  eval "$(zoxide init --cmd cd zsh)"
+  if [[ ! "$(uname -r)" == *[mM]icrosoft* ]]; then
+    eval "$(zoxide init --cmd cd zsh)"
+  fi
 fi
